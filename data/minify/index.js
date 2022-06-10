6db4ed4046beea18e391bc4f57cff387df228763
Cohen Erickson <cohenerickson@gmail.com>
2022-06-10 12:41:33 -0500

    Add Emulators

---
+++ data/minify/index.js
@@ -0,0 +1,24 @@
+const UglifyJS = require("uglify-js");
+const fs = require('fs');
+const uglifycss = require('uglifycss');
+
+if (!String.prototype.replaceAll) {
+    String.prototype.replaceAll = function(a, b) {
+        return this.split(a).join(b);
+    }
+}
+
+var a = fs.readFileSync('../emu-main.js', 'utf8').substring(18);
+var code = fs.readFileSync('../emulator.js', 'utf8').replaceAll('window.EJS_main', a);
+
+function minify(source){
+    var ast = UglifyJS.parse(source); 
+    return UglifyJS.minify(ast).code;
+}
+console.log('minifying');
+var css = fs.readFileSync('../emu-css.css', 'utf8');
+fs.writeFileSync('../emu-css.min.css', uglifycss.processString(css));
+var min = minify(code);
+console.log('done!');
+
+fs.writeFileSync('../emulator.min.js', min);
